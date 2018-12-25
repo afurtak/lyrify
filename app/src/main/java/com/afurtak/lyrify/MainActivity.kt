262838f052acd328e63815c8b0a53c9a25a35a2e
Adam Furtak <adam.furtak@outlook.com>
2018-12-25 19:05:16 +0100

    added cotoutines to project and add kotlin 1.3

--- app/src/main/java/com/afurtak/lyrify/MainActivity.kt
+++ app/src/main/java/com/afurtak/lyrify/MainActivity.kt
@@ -2,6 +2,7 @@ package com.afurtak.lyrify
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
 
