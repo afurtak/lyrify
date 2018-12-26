502788cb2ed789e7ab222f3a2b0bf63ceb406068
Adam Furtak <adam.furtak@outlook.com>
2018-12-26 01:27:19 +0100

    added search for lyrics

--- app/src/main/java/com/afurtak/lyrify/MainActivity.kt
+++ app/src/main/java/com/afurtak/lyrify/MainActivity.kt
@@ -1,13 +1,66 @@
 package com.afurtak.lyrify
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.runBlocking
+import android.view.View
+import android.widget.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var titleInput : EditText
+    private lateinit var artistInput : EditText
+    private lateinit var submitButton : Button
+
+    private lateinit var resultLayout : LinearLayout
+    private lateinit var resultTextView : TextView
+
+    private lateinit var inputFormLayout : LinearLayout
+    private lateinit var downloadingProgressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        inputFormLayout = findViewById(R.id.input_form_layout)
+        downloadingProgressBar = findViewById(R.id.download_progress_bar)
+
+        resultLayout = findViewById(R.id.result_layout)
+        resultTextView = findViewById(R.id.lyrics_text_view)
+
+        titleInput = findViewById(R.id.title_input)
+        artistInput = findViewById(R.id.artist_input)
+        submitButton = findViewById(R.id.subit_button)
+
+        submitButton.setOnClickListener {
+            val title = titleInput.text.toString()
+            val artist = artistInput.text.toString()
+            val song = Song(title, artist)
+
+            LyricsDownloader().execute(song)
+        }
+    }
+
+    inner class LyricsDownloader : AsyncTask<Song, Any, String?>() {
+
+        override fun onPreExecute() {
+            inputFormLayout.visibility = View.GONE
+            downloadingProgressBar.visibility = View.VISIBLE
+        }
+
+        override fun doInBackground(vararg songs: Song): String? {
+            return songs[0].getLyrics()
+        }
+
+        override fun onPostExecute(result: String?) {
+            downloadingProgressBar.visibility = View.GONE
+            resultLayout.visibility = View.VISIBLE
+            if (result != null)
+                resultTextView.text = result
+            else
+                resultTextView.text = "Lyrics did not find"
+        }
+
     }
 }
---
+++ app/src/main/java/com/afurtak/lyrify/Song.kt
@@ -0,0 +1,53 @@
+package com.afurtak.lyrify
+
+import okhttp3.*
+import java.io.IOException
+import java.util.concurrent.CountDownLatch
+
+data class Song(val title: String, val artist: String) {
+
+    fun getLyrics(): String? {
+        val url = createUrlForSong()
+        val htmlCode = getWebPage(url) ?: return null
+        return parseLyrics(htmlCode)
+    }
+
+    private fun createUrlForSong(): String {
+        return "https://www.tekstowo.pl/piosenka,${artist.replace(' ', '_')},${title.replace(' ', '_')}.html"
+    }
+
+    private fun getWebPage(url: String) : String? {
+        val client = OkHttpClient()
+
+        val request = Request.Builder()
+                .url(url)
+                .build()
+
+        var result: String? = null
+        val countDownLatch = CountDownLatch(1)
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                result = null
+                countDownLatch.countDown()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                result = response.body()!!.string()
+                countDownLatch.countDown()
+            }
+        })
+
+        countDownLatch.await()
+
+        return result
+    }
+
+    private fun parseLyrics(fullPageCode: String) : String {
+        return fullPageCode
+                .substringAfter("Tekst piosenki:</h2><br />")
+                .substringBefore("<p>&nbsp;</p>")
+                .trim()
+                .replace("<br />", "")
+    }
+}
