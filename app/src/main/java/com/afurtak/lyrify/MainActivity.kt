0237d8605de9a8799498b6cf7f5c69a5cad39125
Adam Furtak <adam.furtak@outlook.com>
2018-12-27 17:27:32 +0100

    added fab

--- app/src/main/java/com/afurtak/lyrify/MainActivity.kt
+++ app/src/main/java/com/afurtak/lyrify/MainActivity.kt
@@ -3,6 +3,7 @@ package com.afurtak.lyrify
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.view.View
 import android.widget.*
 
@@ -12,6 +13,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var titleInput : EditText
     private lateinit var artistInput : EditText
     private lateinit var submitButton : Button
+    private lateinit var searchForLyricsFromSpotifyButton : Button
+    private lateinit var fab : FloatingActionButton
 
     private lateinit var resultLayout : LinearLayout
     private lateinit var resultTextView : TextView
@@ -25,21 +28,43 @@ class MainActivity : AppCompatActivity() {
 
         inputFormLayout = findViewById(R.id.input_form_layout)
         downloadingProgressBar = findViewById(R.id.download_progress_bar)
+        fab = findViewById(R.id.fab)
 
         resultLayout = findViewById(R.id.result_layout)
         resultTextView = findViewById(R.id.lyrics_text_view)
 
         titleInput = findViewById(R.id.title_input)
         artistInput = findViewById(R.id.artist_input)
-        submitButton = findViewById(R.id.subit_button)
+        submitButton = findViewById(R.id.search_for_lyrics)
+        searchForLyricsFromSpotifyButton = findViewById(R.id.get_spotify_song_lyric)
 
         submitButton.setOnClickListener {
-            val title = titleInput.text.toString()
-            val artist = artistInput.text.toString()
-            val song = Song(title, artist)
-
-            LyricsDownloader().execute(song)
+            searchForLyrics()
         }
+
+        fab.setOnClickListener {
+            titleInput.setText("")
+            artistInput.setText("")
+
+            resultLayout.visibility = View.GONE
+            inputFormLayout.visibility = View.VISIBLE
+        }
+    }
+
+    fun searchForLyrics() {
+        val title = titleInput.text.toString()
+        val artist = artistInput.text.toString()
+        if (title.isEmpty()) {
+            Toast.makeText(this, "Please, enter the title of song", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (artist.isEmpty()) {
+            Toast.makeText(this, "Please, enter the name of artist", Toast.LENGTH_LONG).show()
+            return
+        }
+        val song = Song(title, artist)
+        LyricsDownloader().execute(song)
     }
 
     inner class LyricsDownloader : AsyncTask<Song, Any, String?>() {
